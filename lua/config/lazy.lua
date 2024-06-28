970db25149f01b1c663e8aa8693ba72db22accc7
Steve Chernyak <steve.chernyak@gmail.com>
2024-06-27 22:16:12 -0400

    Plugin updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
+  concurrency = 1,
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
