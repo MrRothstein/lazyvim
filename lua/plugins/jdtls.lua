d6ec1451f55a236d43a446de8937e5437a8068df
Steve Chernyak <steve@Steves-MBP.fios-router.home>
2024-10-21 22:55:10 -0400

    Update JDKs

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -8,15 +8,15 @@ return {
             runtimes = {
               {
                 name = "JavaSE-11",
-                path = "~/.sdkman/candidates/java/11.0.22-tem/",
+                path = "~/.sdkman/candidates/java/11.0.25-tem/",
               },
               {
                 name = "JavaSE-17",
-                path = "~/.sdkman/candidates/java/17.0.10-tem/",
+                path = "~/.sdkman/candidates/java/17.0.17-tem/",
               },
               {
                 name = "JavaSE-21",
-                path = "~/.sdkman/candidates/java/21.0.2-tem/",
+                path = "~/.sdkman/candidates/java/21.0.5-tem/",
               },
             },
           },
