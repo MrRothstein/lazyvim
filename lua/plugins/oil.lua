947c082104e2dae492a71457eeb5cc6c1e4f5563
Steve Chernyak <steve@Steves-MBP.fios-router.home>
2024-10-21 22:27:41 -0400

    Added oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  keys = {
+    { "-", ":Oil<CR>", mode = "n", desc = "Open prent directory with Oil" },
+  },
+}
