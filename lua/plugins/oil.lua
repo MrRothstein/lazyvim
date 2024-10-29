f77c66d7d53c30d74b5afaca354a1523968acb62
MrRothstein <mrrothstein@gmail.com>
2024-10-28 23:55:52 -0400

    Oil config updates

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,17 @@ return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+    columns = {
+      "icon",
+      "permissions",
+      "size",
+      "mtime",
+    },
+  },
   keys = {
     { "-", ":Oil<CR>", mode = "n", desc = "Open prent directory with Oil" },
   },
