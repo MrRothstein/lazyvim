68c3dd4450f5e5f29a18aab9909e30159cdd0604
MrRothstein <mrrothstein@gmail.com>
2024-11-11 21:16:19 -0500

    Disable unused languages

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,5 +5,8 @@ vim.g.mapleader = ","
 
 vim.g.python3_host_prog = "~/.pyenv/versions/.py3nvim/bin/python"
 
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 vim.opt.conceallevel = 0
 vim.opt.foldmethod = "manual"
