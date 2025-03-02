c89220c1647681df53a42ac21caf9f2c952d04b5
stevec <steve.chernyak@gmail.com>
2025-03-02 11:16:05 -0500

    Default snacks explorer to show hidden and ignored files

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,5 +2,14 @@ return {
   "folke/snacks.nvim",
   opts = {
     dashboard = { enabled = false },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+          exclude = { "node_modules", ".git" },
+        },
+      },
+    },
   },
 }
